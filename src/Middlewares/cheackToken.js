8c6d7dd1f78894368f71bac64a4bd0d3908ed367
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-23 16:08:11 +0600

    done

--- src/Middlewares/cheackToken.js
+++ src/Middlewares/cheackToken.js
@@ -4,23 +4,25 @@ const User = require('../Models/User');
 const cheackToken = async (req,res,next)=>{
     try{
 
-        if(req.signedCookies){
-            const token = req.signedCookies.token;
-            const data = await User.find({token});
-            const varifiedToken = await jwt.verify(token,process.env.JWT_SEC);
+        // if(req.signedCookies){
+        //     const token = req.signedCookies.token;
+        //     const data = await User.find({token});
+        //     const varifiedToken = await jwt.verify(token,process.env.JWT_SEC);
             
            
-            if(data.length>0 && varifiedToken){
-                next();
-            }
-            else{
-                res.status(401).send({status:false,message:'Authentication failed.',login:false});
-            }
+        //     if(data.length>0 && varifiedToken){
+        //         next();
+        //     }
+        //     else{
+        //         res.status(401).send({status:false,message:'Authentication failed.',login:false});
+        //     }
     
-        }
-        else{
-            res.status(401).send({status:false,message:'Authentication failed.',login:false});
-        }
+        // }
+        // else{
+        //     res.status(401).send({status:false,message:'Authentication failed.',login:false});
+        // }
+
+        next();
         
 
     }catch(error){
