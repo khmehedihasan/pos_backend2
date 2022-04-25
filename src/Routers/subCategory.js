0a6bb8a15b7f3f38ba7054d64ec290b25ca9e6f2
khmehedihasan <mehedihasan47341@gmail.com>
2022-04-25 07:35:02 +0600

    update

--- src/Routers/subCategory.js
+++ src/Routers/subCategory.js
@@ -8,6 +8,8 @@ const cheackToken = require('../Middlewares/cheackToken');
 
 router.get('/', cheackToken, subCategory.allSubCategory);
 
+router.get('/search', cheackToken, subCategory.searchSubCategory);
+
 router.get('/:id', cheackToken, validObjectId, subCategory.singleSubCategory);
 
 router.post('/', cheackToken, subCategory.addSubCategory)
